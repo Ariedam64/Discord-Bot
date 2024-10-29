27bafce1c75c0e88913fd1faaf058381a5d9699f
Ariedam64 <manitou88.jeux@gmail.com>
2024-10-29 16:27:51 +0100

    Update packages

--- handlers/playerHandler.js
+++ handlers/playerHandler.js
@@ -2,7 +2,6 @@ const { Player } = require('discord-player');
 const { YoutubeiExtractor } = require("discord-player-youtubei")
 
 module.exports = (client) => {
-  //process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
   client.player = new Player(client, {
     ytdlOptions: {
       filter: 'audioonly',
